f4ae69a21815c9d1389c205c6270270271db7177
KamikaziD <druhfus@gmail.com>
2022-11-19 19:59:03 +0200

    Added tests, Ingredient Model, serializer, url, view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -8,7 +8,9 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 from core.models import (
-    Recipe, Tag
+    Recipe,
+    Tag,
+    Ingredient,
 )
 from recipe import serializers
 
@@ -50,4 +52,16 @@ class TagViewSet(
 
     def get_queryset(self):
         """retrieve tags for authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientsViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database"""
+    serializer_class = serializers.IngredientsSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
